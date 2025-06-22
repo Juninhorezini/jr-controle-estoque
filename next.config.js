22bec3d379a243acb1b464505a279390ec3e3cf2
Juninhorezini <juninhorezini@gmail.com>
2025-06-22 04:11:01 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    forceSwcTransforms: true,
   },
-  basePath: '',
-  assetPrefix: '',
-  distDir: 'out'
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
